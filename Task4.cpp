62654fa1c0d2a5d524add4d27090a0386ef478e2
ali <121182316+aliw14@users.noreply.github.com>
2024-04-09 14:06:02 +0400

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdio> 
+
 using namespace std;
 
 int main() {
-    int n[] = {5, 10, 24, 335, 10, 4, 90, 3245, 2, 1, 6, 9};
-    
-    n[5] = 100; 
+    char var[] = "100";
+    int n[12];
+    int i;
 
-    cout << "Array after changing the 6th element to 100:\n";
-    for(int i = 0; i < 12; ++i) {
-        cout << n[i] << " ";
+    for (i = 0; i < 12; i++) {
+        scanf("%d", &n[i]);
+    }
+
+    printf("Elements of array n: ");
+    for (i = 0; i < 12; i++) {
+        printf("%d ", n[i]);
+    }
+
+    n[5] = atoi(var);
+
+    printf("\nArray after changing the 6th element to 100: ");
+    for (int i = 0; i < 12; ++i) {
+        printf("%d ", n[i]);
     }
 
     return 0;
 }
+
